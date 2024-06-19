c1b60a7e773040678f38b3fc587163199c177dac
Alef-Alves-p <alefpalves@gmail.com>
2024-06-19 18:10:01 -0300

    primeiro commit

---
+++ script2.bat
@@ -0,0 +1,15 @@
+@echo off 
+
+rem Limpando a tela
+
+cls
+
+set /p nome=digite seu nome= 
+
+set /p email=digite seu email= 
+
+pause
+
+echo .....................................
+
+echo seu nome é %nome% e seu e-mail é %email%
